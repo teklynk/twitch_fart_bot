95d9a3e7ee2eb501d1564d5d65450c6a0338ee26
teklynk <ryanjones153@gmail.com>
2022-03-28 23:21:11 -0400

    refactored code base. added animated gif option

--- assets/js/farts.js
+++ assets/js/farts.js
@@ -9,14 +9,53 @@ $(document).ready(function () {
     let coolDown = getUrlParameter('cooldown');
     let channelName = getUrlParameter('channel').toLowerCase();
     let modsonly = getUrlParameter('modsonly');
+    let rotation = getUrlParameter('rotation');
+    let animation = getUrlParameter('animation');
     let idleTime = 0;
+    let audioDuration = 1;
+    let numOfFiles = 21;
+
+    if (!rotation) {
+        rotation = "0";
+    }
+
+    if (!animation) {
+        animation = "0";
+    }
 
     let timer = setInterval(timeSleep, 1000); //seconds
+
+    function loadSounds() {
+        let audioElements = "";
+
+        for (let i = 1; i <= numOfFiles; i++) {
+          audioElements += "<audio id='audio-" + i + "' preload='auto'><source src='./media/fart" + i + ".mp3' type='audio/mpeg'></audio>";
+        }
+
+        $(audioElements).appendTo('#container');
+    }
+
+    // Preload all sound files
+    loadSounds();
+
+    function play(item) {
+        let audioPlay = document.getElementById("audio-" + item);
+        audioPlay.play();
+    }
 
     function timeSleep() {
         idleTime = idleTime + 1;
     }
 
+    // create img element. dummy 1x1 pixel gif as a placeholder
+    let fartAnimation = "<img class='responsive' src='data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D'>";
+    $(fartAnimation).appendTo('#container');
+
+    if (rotation) {
+        $("img").css("transform", "rotate(" + rotation + "deg)");
+    }
+
+    // load TMI js
     const client = new tmi.Client({
         channels: [channelName]
     });
@@ -25,7 +64,7 @@ $(document).ready(function () {
 
     client.on('chat', (channel, user, message, self) => {
 
-        //alert message
+        
         if (user['message-type'] === 'chat') {
 
             if (modsonly === 'true' && (user.mod || user.username === channelName)) {
@@ -34,29 +73,37 @@ $(document).ready(function () {
                 playSound(); //everyone
             }
 
+            function createSound(){
+                idleTime = 0;
+                timer = 0;
+                clearInterval(timer);
+
+                let randomFart = Math.floor((Math.random() * numOfFiles) + 1);
+
+                let audio = $("#audio-" + randomFart)[0];
+
+                let audioDuration = audio.duration;
+
+                play(randomFart);
+
+                if (animation !== "0") {
+                    $("#container img").attr("src","./media/fart" + animation + ".gif");
+
+                    $("#container img").fadeIn(100).delay(audioDuration.toFixed(1) * 1000).fadeOut(500, function () {
+                        // dummy 1x1 pixel gif as a placeholder
+                        $(this).attr("src","data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D");
+                    });
+                }
+            }
+
             function playSound() {
 
                 if (message.startsWith("!fart")) {
 
-                    if (idleTime >= coolDown) {
-
-                        console.log(idleTime);
-
-                        idleTime = 0;
-                        timer = 0;
-                        clearInterval(timer);
-
-                        console.log(user.username + " just farted :)");
-
-                        let randomFart = Math.floor((Math.random() * 21) + 1);
-
-                        let audioPlayer = "<audio autoplay><source src='./media/fart" + randomFart + ".mp3' type='audio/mpeg'></audio>";
-
-                        $(audioPlayer).appendTo('#container');
-
-                        $("#container audio").fadeIn(500).delay(5000).fadeOut(500, function () {
-                            $(this).remove();
-                        });
+                    if (user.username === channelName){
+                        createSound(); //streamer only - no cooldown. plays on demand
+                    } else if(idleTime >= parseInt(coolDown)) {
+                        createSound(); //cooldown for everyone else including mods
                     }
 
                 }
